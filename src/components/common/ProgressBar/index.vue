3374163e77cac6128ffb9e44373a3e91837b4494
Morilence <1280659615@qq.com>
2020-08-29 22:03:05 +0800

    feat: replenish the logic of movement for ProgressBar.

--- src/components/common/ProgressBar/index.vue
+++ src/components/common/ProgressBar/index.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="progress-bar wrap" :style="{ width: width + 'px', height: height + 'px' }">
-        <div class="inner" :style="{ height }">
-            <div class="loaded"></div>
-            <div class="played">
-                <div class="thumb" :style="{ width: thumbRadius + 'px', height: thumbRadius + 'px' }">
-                    <i class="dot" :style="{ width: dotRadius + 'px', height: dotRadius + 'px' }"></i>
+        <div class="inner" ref="inner" :style="{ height }" @mousedown="move($event)">
+            <div class="loaded" :style="{ width: loadedProgress + '%' }"></div>
+            <div class="played" :style="{ width: playedProgress + '%' }">
+                <div
+                    class="thumb"
+                    :style="{ width: thumbDiam + 'px', height: thumbDiam + 'px', right: -(thumbDiam / 2) + 'px' }"
+                    @click="slide($event)"
+                >
+                    <i class="dot" :style="{ width: dotDiam + 'px', height: dotDiam + 'px' }"></i>
                 </div>
             </div>
         </div>
@@ -26,19 +30,30 @@ export default {
     },
     data() {
         return {
-            progress: 0
+            loadedProgress: 50,
+            playedProgress: 20
         };
     },
     computed: {
-        thumbRadius: {
+        thumbDiam: {
             get() {
-                return this.height + 6.5;
+                return this.height + 6.6;
             }
         },
-        dotRadius: {
+        dotDiam: {
             get() {
                 return this.height - 1;
             }
+        }
+    },
+    methods: {
+        move(evt) {
+            if (evt.target == this.$refs.inner) {
+                this.playedProgress = (evt.offsetX / this.width) * 100;
+            }
+        },
+        slide(evt) {
+            console.log("distance to the left:", evt.clientX - this.$refs.inner.getBoundingClientRect().left);
         }
     }
 };
@@ -53,6 +68,8 @@ export default {
 
     position relative
 
+    margin-left 10px
+
     user-select none
 
     .inner
@@ -64,31 +81,30 @@ export default {
         position relative
 
         width 100%
-        height 5px
+        height 4.8px
         background-color rgba(0, 0, 8, 0.065)
 
         .loaded
-            width 50%
+            width 0%
             height 100%
             background-color rgba(0, 0, 5, 0.155)
+
+            pointer-events none
 
         .played
             position absolute
 
-            width 20%
+            width 0%
             height 100%
             background-color $second_forecolor
 
-            .thumb
-                display flex
-                flex-direction row
-                justify-content center
-                align-items center
+            pointer-events none
 
+            .thumb
                 position absolute
                 top 50%
                 transform translateY(-50%)
-                right -2px
+                right -7px
 
                 box-sizing content-box
                 border 1px solid rgb(194, 194, 196)
@@ -96,9 +112,17 @@ export default {
                 background-color #fff
 
                 cursor pointer
+                pointer-events auto
 
                 .dot
+                    position absolute
+                    top 50%
+                    left 50%
+                    transform translate(-50%, -50%)
+
                     border none
                     border-radius 100%
                     background-color $second_forecolor
+
+                    pointer-events none
 </style>
